747648ae31db2c20b7b3bc864c0bc094f2bb243b
liyongxiang <liyongxiang@hoolai.com>
2017-03-13 09:09:59 +0800

    加入中文注释

--- app/src/main/java/frogermcs/io/githubclient/AppComponent.java
+++ app/src/main/java/frogermcs/io/githubclient/AppComponent.java
@@ -14,6 +14,8 @@ import frogermcs.io.githubclient.ui.activity.module.SplashActivityModule;
  */
 //@Component:Component从根本上来说就是一个注入器，也可以说是@Inject和@Module的桥梁，它的主要作用就是连接这两个部分。
 // 将Module中产生的依赖对象自动注入到需要依赖实例的Container中。
+//一个Component可以添加多个Module，这样Component获取依赖时候会自动从多个Module中查找获取。添加多个Module有两种方法，
+// 一种是在Component的注解@Component(modules={××××，×××})中添加多个modules
 @Singleton
 @Component(
         modules = {// 指明Component查找Module的位置
--- app/src/main/java/frogermcs/io/githubclient/AppModule.java
+++ app/src/main/java/frogermcs/io/githubclient/AppModule.java
@@ -6,6 +6,7 @@ import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import frogermcs.io.githubclient.data.api.GithubApiModule;
 import frogermcs.io.githubclient.utils.AnalyticsManager;
 import frogermcs.io.githubclient.utils.Validator;
 
@@ -13,6 +14,9 @@ import frogermcs.io.githubclient.utils.Validator;
  * Created by Miroslaw Stanek on 22.04.15.
  */
 // Module类里面的方法专门提供依赖，所以我们定义一个类，用@Module注解，这样Dagger在构造类的实例的时候，就知道从哪里去找到需要的依赖。
+//一个Component可以添加多个Module，这样Component获取依赖时候会自动从多个Module中查找获取。添加多个Module有两种方法
+//另外一种添加多个Module的方法可以使用@Module的 includes的方法（includes={××××，×××}）
+//@Module(includes = {GithubApiModule.class})
 @Module
 public class AppModule {
     private Application application;
